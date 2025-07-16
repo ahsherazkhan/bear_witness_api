bb0467d3d33b5325e888492719ba8272783061b3
ahshserazkhan <ahshserazkhan@gmail.com>
2025-07-17 02:16:20 +0500

    updated files

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['static.rocket.new', 'application.rocket.new'],
   },
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
+  // Ensure proper output
+  output: 'standalone',
 }
 
 module.exports = nextConfig 
